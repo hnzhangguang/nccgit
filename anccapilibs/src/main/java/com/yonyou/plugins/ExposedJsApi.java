ce6684601e63a0869d695fab46b3e17f93939d2f
zhangguang <zhangguang@yonyou.com>
2020-09-08 10:19:49 +0800

    支持 lambda 表达式

--- anccapilibs/src/main/java/com/yonyou/plugins/ExposedJsApi.java
+++ anccapilibs/src/main/java/com/yonyou/plugins/ExposedJsApi.java
@@ -37,65 +37,65 @@ import java.lang.ref.WeakReference;
  * @Author zhangg
  **/
 public class ExposedJsApi {
-	private static final String TAG = "mmmmExposedJsApi";
-	private static final String MTL_ANDROID = " mtlAndroid";
-	
-	private WeakReference<Activity> mCtx = null;
-	private WeakReference<WebView> mWebView = null;
-	private APIInvoker invoker;
-	
-	public ExposedJsApi(Activity _ctx, WebView _webView) {
-		if (_ctx == null) {
-			MTLLog.w(TAG, "ERROR - ctx is not activity");
-		}
-		mWebView = new WeakReference<WebView>(_webView);
-		String ua = mWebView.get().getSettings().getUserAgentString();
-		mWebView.get().getSettings().setUserAgentString(ua + MTL_ANDROID);
-		mCtx = new WeakReference<Activity>(_ctx);
-		invoker = new APIInvoker(_ctx);
-	}
-	
-	
-	@JavascriptInterface
-	public String callSync(String name, String params) {
-		try {
-			return invoker.call(mCtx.get(), name, params, null, true);
-		} catch (MTLException e) {
-			e.printStackTrace();
-		}
-		return "error";
-	}
-	
-	@JavascriptInterface
-	public String call(String name, String params, String callback) {
-		ApiCallback mtlCallback = new ApiCallback(this, callback);
-		try {
+    private static final String TAG = "mmmmExposedJsApi";
+    private static final String NCC_ANDROID = " nccAndroid";
+
+    private WeakReference<Activity> mCtx = null;
+    private WeakReference<WebView> mWebView = null;
+    private APIInvoker invoker;
+
+    public ExposedJsApi(Activity _ctx, WebView _webView) {
+        if (_ctx == null) {
+            MTLLog.w(TAG, "ERROR - ctx is not activity");
+        }
+        mWebView = new WeakReference<WebView>(_webView);
+        String ua = mWebView.get().getSettings().getUserAgentString();
+        mWebView.get().getSettings().setUserAgentString(ua + NCC_ANDROID);
+        mCtx = new WeakReference<Activity>(_ctx);
+        invoker = new APIInvoker(_ctx);
+    }
+
+
+    @JavascriptInterface
+    public String callSync(String name, String params) {
+        try {
+            return invoker.call(mCtx.get(), name, params, null, true);
+        } catch (MTLException e) {
+            e.printStackTrace();
+        }
+        return "error";
+    }
+
+    @JavascriptInterface
+    public String call(String name, String params, String callback) {
+        ApiCallback mtlCallback = new ApiCallback(this, callback);
+        try {
 //			String callbackId = mtlCallback.getCallbackId();
 //			if (TextUtils.isEmpty(callbackId)) {
 //				mtlCallback.setCallbackId(callback.replace("()", ""));
 //			}
-			invoker.call(mCtx.get(), name, params, mtlCallback, false);
-		} catch (MTLException e) {
-			mtlCallback.error(e.getMessage());
-			e.printStackTrace();
-		}
-		return "success";
-	}
-	
-	
-	public void callback(final String method_callbackId, final JSONObject rs) {
-		if (mCtx.get() == null) {
-			return;
-		}
-		if (mWebView.get() == null) {
-			return;
-		}
-		
-		MTLLog.d("yyy", "execute callback  : " + method_callbackId);
-		mCtx.get().runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				// old method
+            invoker.call(mCtx.get(), name, params, mtlCallback, false);
+        } catch (MTLException e) {
+            mtlCallback.error(e.getMessage());
+            e.printStackTrace();
+        }
+        return "success";
+    }
+
+
+    public void callback(final String method_callbackId, final JSONObject rs) {
+        if (mCtx.get() == null) {
+            return;
+        }
+        if (mWebView.get() == null) {
+            return;
+        }
+
+        MTLLog.d("yyy", "execute callback  : " + method_callbackId);
+        mCtx.get().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // old method
 //				String callback = method.replace("()", "(" + frs + ")");
 //				callback = callback.replaceAll("%5C", "/");
 //				callback = callback.replaceAll("%0A", "");
@@ -105,27 +105,27 @@ public class ExposedJsApi {
 //				} else {
 //					mWebView.get().loadUrl("javascript:" + script);
 //				}
-				
-				try {
-					String data = rs.toString();
-					JSONObject jsonObject = new JSONObject(data);
-					jsonObject.put("callbackId", method_callbackId);  // 添加回调方法
-					data = jsonObject.toString();
-					// new  method
-					String jsCode = String.format("%s(%s,%s)", "window.JSSDK.receiveNativeMessage", "'" + method_callbackId + "'", data);
-					jsCode = jsCode.replaceAll("%5C", "/");
-					jsCode = jsCode.replaceAll("%0A", "");
-					String script = "try{" + jsCode + "}catch(e){console.error(e)}";
+
+                try {
+                    String data = rs.toString();
+                    JSONObject jsonObject = new JSONObject(data);
+                    jsonObject.put("callbackId", method_callbackId);  // 添加回调方法
+                    data = jsonObject.toString();
+                    // new  method
+                    String jsCode = String.format("%s(%s,%s)", "window.JSSDK.receiveNativeMessage", "'" + method_callbackId + "'", data);
+                    jsCode = jsCode.replaceAll("%5C", "/");
+                    jsCode = jsCode.replaceAll("%0A", "");
+                    String script = "try{" + jsCode + "}catch(e){console.error(e)}";
 //					Log.e(TAG, "script: " + script);
-					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-						mWebView.get().evaluateJavascript(script, null);
-					} else {
-						mWebView.get().loadUrl("javascript:" + script);
-					}
-				} catch (Exception e) {
-					Log.e(TAG, e.toString());
-				}
-			}
-		});
-	}
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        mWebView.get().evaluateJavascript(script, null);
+                    } else {
+                        mWebView.get().loadUrl("javascript:" + script);
+                    }
+                } catch (Exception e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        });
+    }
 }
