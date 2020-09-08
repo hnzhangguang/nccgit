c85dc1877c0826c62fe4b891b9c62871ba1e0e87
zhangguang <zhangguang@yonyou.com>
2020-09-08 20:19:49 +0800

    支持 lambda 表达式

--- anccapilibs/src/main/java/com/yonyou/plugins/register/RegisterApiInvoker.java
+++ anccapilibs/src/main/java/com/yonyou/plugins/register/RegisterApiInvoker.java
@@ -1,20 +1,12 @@
 package com.yonyou.plugins.register;
 
 import android.text.TextUtils;
-import android.util.Log;
 
-import com.yonyou.common.constant.Constant;
-import com.yonyou.common.utils.LitePal;
 import com.yonyou.common.utils.logs.LogerNcc;
-import com.yonyou.common.utils.user.UserUtil;
-import com.yonyou.common.utils.utils.CheckUtil;
 import com.yonyou.common.vo.JsonObjectEx;
 import com.yonyou.plugins.IApiInvoker;
 import com.yonyou.plugins.MTLArgs;
 import com.yonyou.plugins.MTLException;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class RegisterApiInvoker implements IApiInvoker {
 
@@ -22,7 +14,7 @@ public class RegisterApiInvoker implements IApiInvoker {
     private static final String rightBtnCallback = "rightcallback";
     private static final String scanCallback = "scancallback";
 
-    private static String leftCallbackName = "";
+    public static String leftCallbackName = "";
 
 
     @Override
