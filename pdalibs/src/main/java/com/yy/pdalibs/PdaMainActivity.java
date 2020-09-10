4af28db649d1628ac340583e417bfa995ae3e20d
zhangguang <zhangguang@yonyou.com>
2020-09-10 09:06:21 +0800

    支持 lambda 表达式

---
+++ pdalibs/src/main/java/com/yy/pdalibs/PdaMainActivity.java
@@ -0,0 +1,14 @@
+package com.yy.pdalibs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PdaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pda_main);
+    }
+}
