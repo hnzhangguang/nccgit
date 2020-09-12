3608b467204d0be2591937e13cc00957838f6495
zhangguang <zhangguang@yonyou.com>
2020-09-12 15:34:22 +0800

    支持 lambda 表达式

--- aall/src/main/java/com/yonyou/nccmob/message/history/NoTODOFragment.java
+++ aall/src/main/java/com/yonyou/nccmob/message/history/NoTODOFragment.java
@@ -159,7 +159,7 @@ public class NoTODOFragment extends BaseFragment {
 
         try {
             String data = NccMsgUtil.getData();
-            NccMsgUtil.updateAllMessageDB(data);// 更新数据
+//            NccMsgUtil.updateAllMessageDB(data);// 更新数据
             List<MessageVO> messageVosByType = NccMsgUtil.getMessageVosByTypeFromDB(Constant.todo_msg);
             todoList.clear();
             todoList.addAll(messageVosByType);
