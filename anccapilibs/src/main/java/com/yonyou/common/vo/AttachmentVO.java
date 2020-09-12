71d55f47b32548af3a5cc6b54184442e526e55fd
zhangguang <zhangguang@yonyou.com>
2020-09-12 13:26:15 +0800

    支持 lambda 表达式

--- anccapilibs/src/main/java/com/yonyou/common/vo/AttachmentVO.java
+++ anccapilibs/src/main/java/com/yonyou/common/vo/AttachmentVO.java
@@ -5,6 +5,8 @@ import org.litepal.crud.LitePalSupport;
 
 import java.io.Serializable;
 
+import javax.xml.validation.Validator;
+
 /*
  * @功能: 消息附件实体
  * @Date  2020/8/13;
@@ -18,6 +20,15 @@ public class AttachmentVO extends LitePalSupport implements Serializable {
     private String downurl;
     private String filename;
     private String type;
+    private String pk_parent;
+
+    public String getPk_parent() {
+        return pk_parent;
+    }
+
+    public void setPk_parent(String pk_parent) {
+        this.pk_parent = pk_parent;
+    }
 
     public String getPk_attachment() {
         return pk_attachment;
@@ -66,6 +77,7 @@ public class AttachmentVO extends LitePalSupport implements Serializable {
         sb.append(", pk_file='").append(pk_file).append('\'');
         sb.append(", downurl='").append(downurl).append('\'');
         sb.append(", filename='").append(filename).append('\'');
+        sb.append(", pk_parent='").append(pk_parent).append('\'');
         sb.append(", type='").append(type).append('\'');
         sb.append('}');
         return sb.toString();
